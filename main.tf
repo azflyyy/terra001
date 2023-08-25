021892e4c7b9eb7ee28f35fd3eeec8251b6df348
aztea <aziza.ettahiri.754918@gmail.com>
2023-08-25 14:45:46 +0100

    latest changes

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "azurerm_app_service_plan" "example" {
   name                = "example-appserviceplan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  kind                = "Linux"
+  kind                = "windows"
   
   sku {
     tier = "Standard"
